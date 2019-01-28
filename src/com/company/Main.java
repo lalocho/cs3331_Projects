7db1a197027479a71b89bf17e4a596ff6c510c84
Luis <lalocho13@gmail.com>
2019-01-28 09:39:29 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public static double current_price(){
+        return
+    }
+    public static double initial_price(){
+
+    }
+    public static double percent_difference(){
+
+    }
+}
